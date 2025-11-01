2790216fefdd8ee9c50ba23c05c61c0bdc00746e
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-11-01 05:58:57 -0400

    archivo de salidad de exportacion de los demas archivos de logica

---
+++ www/js/database/index.js
@@ -0,0 +1,7 @@
+export * from './connection.js';
+export * from './categories.js';
+export * from './products.js';
+export * from './sales.js';
+export * from './expenses.js';
+export * from './config.js';
+export * from './notifications.js';
